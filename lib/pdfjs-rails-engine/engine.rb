22c33e7a63f96ac5d1c68469ceab5e900266f531
Igor Kirichenko <iki@jetthoughts.com>
2016-02-11 16:26:53 +0200

    Added static assets.

--- lib/pdfjs-rails-engine/engine.rb
+++ lib/pdfjs-rails-engine/engine.rb
@@ -1,5 +1,8 @@
 module PdfjsRailsEngine
   class Engine < ::Rails::Engine
     isolate_namespace PdfjsRailsEngine
+    initializer "static assets" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+    end
   end
 end
