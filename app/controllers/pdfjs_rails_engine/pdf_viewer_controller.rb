6e633bedaac2a5a303ebfc8baad5b93e67e19a8d
Sergey Ryabov <connacte@gmail.com>
2017-04-06 00:11:25 +0300

    add Access-Control headers to iframe request for a worker

--- app/controllers/pdfjs_rails_engine/pdf_viewer_controller.rb
+++ app/controllers/pdfjs_rails_engine/pdf_viewer_controller.rb
@@ -1,10 +1,16 @@
 module PdfjsRailsEngine
   class PdfViewerController < ApplicationController
     layout 'pdfjs_rails_engine/application'
-    
-    def index
+    before_filter :additional_headers
 
+    def index
     end
-    
+
+    def additional_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
   end
 end
